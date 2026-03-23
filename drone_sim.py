41dcfcc4479abb83b4ba662c263838fe1ff5bd37
Monika <monikar_icb2023@ksit.edu.in>
2026-03-23 23:51:08 +0530

    Add socket communication for drone telemetry data

--- drone_sim.py
+++ drone_sim.py
@@ -1,1 +1,30 @@
+import socket
+import json
+import time
 
+HOST = "127.0.0.1"
+PORT = 5000
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.connect((HOST,PORT))
+
+lat = 12.9716
+lon = 77.5946
+altitude = 0
+battery = 100
+
+while True:
+  data = {
+    "gps": {"lat":lat,"lon":lon},
+    "altitude":altitude,
+    "battery": battery
+  }
+sock.sendall((json.dumps(data)+"\n").encode())
+
+#simulate changes
+lat += 0.0001
+lon +=0.0001
+altitude += 1
+battery -= 0.1
+
+time.sleep(1)
