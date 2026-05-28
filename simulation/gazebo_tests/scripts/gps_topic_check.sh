b8131d20b4e2acbf3f48ec2971d78cc162591f6f
Monika <monikar_icb2023@ksit.edu.in>
2026-05-29 03:53:30 +0530

    Update gps_topic_check.sh

--- simulation/gazebo_tests/scripts/gps_topic_check.sh
+++ simulation/gazebo_tests/scripts/gps_topic_check.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo "Checking GPS topic..."
-rostopic echo /gps/fix
+
+echo "Checking GPS topic availability..."
+
+ros2 topic list | grep gps
+
+echo ""
+echo "Streaming GPS telemetry..."
+echo "Press CTRL+C to stop"
+
+ros2 topic echo /gps/fix
